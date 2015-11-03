7fa327d14d4461f873e978494fd78159b19aaa65
qinghaibo <qinghaibo@ubankers.com>
2015-11-03 09:51:02 +0800

    revise server __dirname

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/app/')));
+app.use(express.static(path.join(__dirname, '../client/')));
 app.use('/api', routes);
 
 // catch 404 and forward to error handler
